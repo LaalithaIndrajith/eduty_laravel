6b6cd54f460000771c7250b335ba8cfd92b51643
Laalitha <intern5.codefix@gmail.com>
2021-07-05 01:40:16 +0530

    Permission creation is done with notifications

---
+++ app/Http/Controllers/PermissionController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Exception;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Spatie\Permission\Models\Permission;
+
+class PermissionController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware(['auth']);
+    }
+    
+    public function index(){
+        $page_breadcrumbs = [
+            'main_module' =>  [   
+                'title' => 'Configurations',
+                'page' => '#',
+            ],
+            'sub_module' =>  [   
+                'title' => 'Access & Permissions',
+                'page' => '#',
+            ],
+        ];
+
+        $page_title = 'Create Permission';
+
+        return view('pages.configurations.create_permission', compact('page_title','page_breadcrumbs'));
+    }
+
+    public function createPermission(Request $request){
+        try{
+            Permission::create(['name' =>  $request->permission_name]);
+            $permissionCreation = [
+                'msg' =>  'Permission Created Successfully',
+                'title' => 'Permission Creation',
+                'status' =>  1,
+            ];
+
+            $request->session()->flash('permissionCreation', $permissionCreation);
+
+        }catch(Exception $e){
+            $permissionCreation = [
+                'msg' =>  $e->getMessage(),
+                'title' => 'Permission Creation is unsuccessful',
+                'status' =>  0,
+            ];
+
+            $request->session()->flash('permissionCreation', $permissionCreation);
+        }
+        return redirect()->route('PermissionCreationView');
+    }
+
+    public function fetchPermissionsToDrawTbl(){
+        $permissions = DB::table('permissions')->get();
+        $data = array();
+        foreach($permissions as $permission){
+            $d = array();
+            $d['permissionName'] = $permission->name;
+            $d['createdAt']    = $permission->created_at;
+            $d['updatedAt']    = $permission->updated_at;
+            $d['id']           = $permission->id;
+            array_push($data, $d);
+        }
+
+        return ['data' => $data];
+      
+    }
+}
