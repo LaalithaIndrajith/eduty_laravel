da54a2b5512c1e650706cd1ff3e5991081b3928e
Laalitha <intern5.codefix@gmail.com>
2021-07-15 00:08:29 +0530

    Add Mail Facility by installing Mailjet package and done some necessary configurations

--- config/app.php
+++ config/app.php
@@ -177,6 +177,8 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Spatie\Permission\PermissionServiceProvider::class,
+        Mailjet\LaravelMailjet\MailjetServiceProvider::class,
+        Mailjet\LaravelMailjet\MailjetMailServiceProvider::class,
 
     ],
 
@@ -233,6 +235,8 @@ return [
         // Metronic Aliases
         'Metronic' => App\Classes\Theme\Metronic::class,
         'Menu' => App\Classes\Theme\Menu::class,
+
+        'Mailjet' => Mailjet\LaravelMailjet\Facades\Mailjet::class,
 
     ],
 
